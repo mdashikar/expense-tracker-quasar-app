7f93e32dd8105e25f08d24c273c109ec06cf6f4c
Md Ashiqur Rahman <md.ashik.ar@gmail.com>
2020-09-22 03:34:15 +0600

    chore: gsap and tailwindcss added

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
+  <!-- This is an example component -->
+  <div class="h-screen w-screen bg-gray-100 flex items-center">
+    <div class="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
+        <div class="max-w-md">
+            <div class="text-5xl font-dark font-bold">404</div>
+              <p
+                class="text-2xl md:text-3xl font-light leading-normal"
+              >Sorry we couldn't find this page. </p>
+            <p class="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+            <q-btn
+            class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700"
+            color="white"
+            text-color="blue"
+            unelevated
+            to="/"
+            label="back to homepage"
+            no-caps
+          />
       </div>
-
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+      <div class="max-w-md ml-4">
+          <img src="~/assets/page_not_found_su7k.svg" alt="Not Found" />
       </div>
-
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+      
     </div>
   </div>
 </template>
@@ -27,3 +31,8 @@ export default {
   name: 'Error404'
 }
 </script>
+<style scoped>
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+</style>
