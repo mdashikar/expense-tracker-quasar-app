6ad3f298d284b11e5eec78813a48c34701266dc7
Md Ashiqur Rahman <md.ashik.ar@gmail.com>
2020-09-22 23:15:40 +0600

    chore: added firebase and vuex store for firebase auth

---
+++ firebase.conf.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyC0ot00yiq1xxkJW5Nar6RA6dygJvVIpzI",
+    authDomain: "expense-tracker-c71eb.firebaseapp.com",
+    databaseURL: "https://expense-tracker-c71eb.firebaseio.com",
+    projectId: "expense-tracker-c71eb",
+    storageBucket: "expense-tracker-c71eb.appspot.com",
+    messagingSenderId: "905644649800",
+    appId: "1:905644649800:web:0465d5201407e7b4ff977d",
+    measurementId: "G-HKFBZWVMM3"
+}
