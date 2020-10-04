6f2dc13199dfed7d905fb2a61d26e1085bf2d85d
Md Ashiqur Rahman <md.ashik.ar@gmail.com>
2020-10-04 23:59:57 +0600

    added dashboard layout

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,13 +1,185 @@
 <template>
   <q-page class="q-ma-lg root">
     <div class="grid grid-cols-5 gap-0">
-        <div class="col-span-4 bg-gray-400 left-side">
-          <h2 class="text-3xl font-bold text-blue-800">Expense Tracker</h2>
-        </div>
-        <div class="col-span-1 bg-gray-200 right-side">
-          <h2 class="text-3xl font-bold text-dark">Wallet </h2>
-        </div>
+      <div class="col-span-4 bg-gray-400 left-side px-6 ">
+        <q-icon size="lg" name="local_mall" color="text-white" />
+        <h2 class="text-3xl font-bold text-blue-800 ">Home Wallet</h2>
+        <section class="text-white body-font">
+          <div class="container px-2 py-10 mx-auto">
+            <div class="flex flex-col text-center w-full mb-20">
+              <h1
+                class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900"
+              >
+                Our Team
+              </h1>
+              <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+                Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+                gentrify, subway tile poke farm-to-table. Franzen you probably
+                haven't heard of them.
+              </p>
+            </div>
+            <div class="flex flex-wrap -m-2">
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div
+                  class="h-full flex items-center border-gray-200 border p-4 rounded-lg"
+                >
+                  <img
+                    alt="team"
+                    class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/80x80"
+                  />
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Holden Caulfield
+                    </h2>
+                    <p class="text-gray-500">UI Designer</p>
+                  </div>
+                </div>
+              </div>
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div
+                  class="h-full flex items-center border-gray-200 border p-4 rounded-lg"
+                >
+                  <img
+                    alt="team"
+                    class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/84x84"
+                  />
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Henry Letham
+                    </h2>
+                    <p class="text-gray-500">CTO</p>
+                  </div>
+                </div>
+              </div>
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div
+                  class="h-full flex items-center border-gray-200 border p-4 rounded-lg"
+                >
+                  <img
+                    alt="team"
+                    class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/88x88"
+                  />
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Oskar Blinde
+                    </h2>
+                    <p class="text-gray-500">Founder</p>
+                  </div>
+                </div>
+              </div>
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div
+                  class="h-full flex items-center border-gray-200 border p-4 rounded-lg"
+                >
+                  <img
+                    alt="team"
+                    class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/90x90"
+                  />
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      John Doe
+                    </h2>
+                    <p class="text-gray-500">DevOps</p>
+                  </div>
+                </div>
+              </div>
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div
+                  class="h-full flex items-center border-gray-200 border p-4 rounded-lg"
+                >
+                  <img
+                    alt="team"
+                    class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/94x94"
+                  />
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Martin Eden
+                    </h2>
+                    <p class="text-gray-500">Software Engineer</p>
+                  </div>
+                </div>
+              </div>
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div
+                  class="h-full flex items-center border-gray-200 border p-4 rounded-lg"
+                >
+                  <img
+                    alt="team"
+                    class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/98x98"
+                  />
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Boris Kitua
+                    </h2>
+                    <p class="text-gray-500">UX Researcher</p>
+                  </div>
+                </div>
+              </div>
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div
+                  class="h-full flex items-center border-gray-200 border p-4 rounded-lg"
+                >
+                  <img
+                    alt="team"
+                    class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/100x90"
+                  />
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Atticus Finch
+                    </h2>
+                    <p class="text-gray-500">QA Engineer</p>
+                  </div>
+                </div>
+              </div>
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div
+                  class="h-full flex items-center border-gray-200 border p-4 rounded-lg"
+                >
+                  <img
+                    alt="team"
+                    class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/104x94"
+                  />
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Alper Kamu
+                    </h2>
+                    <p class="text-gray-500">System</p>
+                  </div>
+                </div>
+              </div>
+              <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                <div
+                  class="h-full flex items-center border-gray-200 border p-4 rounded-lg"
+                >
+                  <img
+                    alt="team"
+                    class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/108x98"
+                  />
+                  <div class="flex-grow">
+                    <h2 class="text-gray-900 title-font font-medium">
+                      Rodrigo Monchi
+                    </h2>
+                    <p class="text-gray-500">Product Manager</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
+      <div class="col-span-1 bg-gray-200 right-side px-6">
+        <h2 class="text-3xl font-bold text-blue-900">Wallet</h2>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -32,13 +204,12 @@ export default {
 @import "tailwindcss/components";
 @import "tailwindcss/utilities";
 
-.root{
+.root {
   margin: 0;
   overflow-x: hidden;
-  .left-side, .right-side{
+  .left-side,
+  .right-side {
     height: 100vh;
   }
 }
-
-
 </style>
